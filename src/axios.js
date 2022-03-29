b581ae0d6bf2c4ec1374c4f5b1fbf7fa9db1759a
Tariq Magsi <tariqmagsi125@gmail.com>
2022-03-29 10:34:57 +0200

    updated server var

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_LOCAL_SERVER_BASE_URL;
+const baseURL = process.env.REACT_APP_SERVER_BASE_URL;
 
 const instance = axios.create({
     baseURL,
